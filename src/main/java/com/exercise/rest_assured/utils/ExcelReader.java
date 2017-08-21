7d40524525a455ce02410baf45100ee071bd7815
sam <sam@DESKTOP-JGBK7IM>
2017-08-21 22:51:18 +0800

    去掉static变量

--- src/main/java/com/exercise/rest_assured/utils/ExcelReader.java
+++ src/main/java/com/exercise/rest_assured/utils/ExcelReader.java
@@ -12,61 +12,36 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.Test;
 
 public class ExcelReader {
-	
-	static String filePath = null;
-	static String sheetName = null;
-	static String caseName = null;
-	
-	static Workbook wb = null;
-	static Sheet sheet = null;
-	
-	static HashMap<String, String> map = new HashMap<String, String>();
+	private HashMap<String, String> map = null;
 	
 	public ExcelReader() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * @param path 文件路径
+	 * @param sheetName sheet表名
+	 * @param caseName 用例名
+	 * */
 	public ExcelReader(String path,String sheetName,String caseName) {
 		// TODO Auto-generated constructor stub
-		setPath(path);
-		setSheetName(sheetName);
-		setCaseName(caseName);
+		map = mapFromSheet(path, sheetName, caseName);
 	}
 	
-	/**
-	 * 设置Excel文件路径
-	 * @param excelFilePath Excel文件路径
-	 * */
-	public void setPath(String excelFilePath){
-		this.filePath = excelFilePath;
-	}
-	
-	/**
-	 * 设置Sheet名称
-	 * @param sheetName Excel sheet表名称
-	 * */
-	public void setSheetName(String sheetName){
-		this.sheetName = sheetName;
-	}
-	
-	/**
-	 * 设置用例名称
-	 * @param caseName 用例名称
-	 * */
-	public void setCaseName(String caseName){
-		this.caseName = caseName;
+	public HashMap<String, String> getRowMap(){
+		return map;
 	}
 	
 	/**
 	 * 创建工作簿对象
-	 * @param filePath
+	 * @param filePath Excel文件路径
 	 * @return
 	 * @throws IOException
 	 */
 	public static final Workbook setWorkbook(String filePath) throws IOException {
+		Workbook wb = null;
 		if(StringUtils.isBlank(filePath)) {
 			throw new IllegalArgumentException("参数错误!!!") ;
 		}
@@ -80,41 +55,29 @@ public class ExcelReader {
 			throw new IllegalArgumentException("不支持除：xls/xlsx以外的文件格式!!!") ;
 		}
 	}
-	
-	public Workbook getWorkBook(){
-		return wb;
-	}
-	
-	public static Sheet getSheet(String sheetName){
-		return wb.getSheet(sheetName);
-	}
-	
-	public HashMap<String, String> getRowMap(){
-		return map;
-	}
-	
+
 	/**
 	 * 获取指定行的数据
-	 * @param Workbook wb 
-	 * @param Sheet sheet
+	 * @param fileName 文件名(全路径)
+	 * @param sheetName sheet表名
+	 * @param caseName 用例名
 	 * @return HashMap<key,value> key:首行cell的值，value:指定行cell的值
 	 * */
 	public HashMap<String, String> mapFromSheet(String fileName, String sheetName, String caseName){
 		
-//		Workbook wookbook = null;
-//		Sheet sheet = null;
+		Workbook workbook = null;
+		Sheet sheet = null;
 		int rowNum = 0;
 		
 		HashMap<String, String> map = new HashMap<String, String>();
-		
 		try {
-			setWorkbook(fileName);
+			workbook = setWorkbook(fileName);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		sheet = getSheet(sheetName);
+		sheet = workbook.getSheet(sheetName);
 		
 		for (int i = sheet.getFirstRowNum(); i < sheet.getLastRowNum()+1; i++) {
 			
@@ -133,58 +96,15 @@ public class ExcelReader {
 			if (sheet.getRow(0).getCell(i).toString()==null) {
 				throw new IllegalArgumentException("参数错误："+fileName+"表："+sheetName+"第1行第"+i+"列的值为空!!!") ;
 			}
-
-			map.put(sheet.getRow(0).getCell(i).toString(), sheet.getRow(rowNum).getCell(i).toString());
-		}
-		
-		return map;
-	}
-	
-	/**
-	 * 获取指定行的数据
-	 * @param Workbook wb 
-	 * @param Sheet sheet
-	 * @return HashMap<key,value> key:首行cell的值，value:指定行cell的值
-	 * */
-	public HashMap<String, String> mapFromSheet(){
-		
-//		Workbook wookbook = null;
-//		Sheet sheet = null;
-		int rowNum = 0;
-		
-		for (int i = sheet.getFirstRowNum(); i < sheet.getLastRowNum()+1; i++) {
 			
-			Row row = sheet.getRow(i);
-			if (row.getLastCellNum()==0) {
-				continue;
+			if (sheet.getRow(rowNum).getCell(i)==null) {
+				map.put(sheet.getRow(0).getCell(i).toString(), null);
+			} else {
+				map.put(sheet.getRow(0).getCell(i).toString(), sheet.getRow(rowNum).getCell(i).toString());
 			}
-
-			if (row.getCell(0).toString().equals(caseName)) {
-				rowNum = i;
-				break;
-			}
-		}
-		
-		for (int i = 0; i < sheet.getRow(0).getLastCellNum(); i++) {
-			if (sheet.getRow(0).getCell(i).toString()==null) {
-				throw new IllegalArgumentException("参数错误："+filePath+"表："+sheetName+"第1行第"+i+"列的值为空!!!") ;
-			}
-
-			map.put(sheet.getRow(0).getCell(i).toString(), sheet.getRow(rowNum).getCell(i).toString());
+			
 		}
 		
 		return map;
-	}
-	
-	@Test
-	public void getMapTest(){
-		String path = System.getProperty("user.dir");
-		String filePath = path + "\\resources\\case\\Basic.xlsx";
-		String sheetName = "Base";
-		String caseName = "Basic";
-		HashMap<String, String> map = null;
-		map = mapFromSheet(filePath, sheetName, caseName);
-		
-		System.out.println(map.get("Method"));
 	}
 }
