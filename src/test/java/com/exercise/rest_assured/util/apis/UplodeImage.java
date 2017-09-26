351ad5bb22e9eea75ca5d728db0147d4a869c063
sam <sam@DESKTOP-F5F253E>
2017-09-26 10:51:45 +0800

    上传图片接口

---
+++ src/test/java/com/exercise/rest_assured/util/apis/UplodeImage.java
@@ -0,0 +1,35 @@
+package com.exercise.rest_assured.util.apis;
+
+import java.io.File;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import junit.framework.Assert;
+
+import static io.restassured.RestAssured.given;
+
+public class UplodeImage {
+	
+	public String upload(String baseURL,File file){
+		System.out.println(file.getAbsolutePath());
+		if(!file.exists()){
+			Assert.fail("File not found:"+file.getAbsolutePath());
+		}
+		
+		Response response = given()
+			.multiPart("image",file.getName(),"text/plain")
+			.multiPart("image",file,"image/jpeg")
+		.when()
+			.post(baseURL+"/images/uplodeimage")
+		.then()
+		.extract()
+			.response();
+		String body = response.getBody().asString();
+		while(body.charAt(0)!='{'){
+			body = body.substring(1, body.length());
+		}
+		JsonPath json = JsonPath.with(body).setRoot("value");
+		
+		return json.getString("imgurl");
+	}
+}
